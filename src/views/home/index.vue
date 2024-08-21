ab0dd2bd686932d863bacea965e0df0955591ed7
hboot <bobolity@163.com>
2024-08-22 00:26:44 +0800

    feat: socket通信接收到数据，页面展示渲染

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,25 +9,37 @@
 
     <div class="version-list">
       <h3>dependencies</h3>
-      <div class="version-item" v-for="name in dependencies" :key="name">
+      <div
+        class="version-item"
+        v-for="name in formatValues(dependencies)"
+        :key="name"
+      >
         <div class="name">{{ name }}</div>
+
         <div
           class="version"
-          v-for="info in ata.dependencies[name]"
+          v-for="info in dependencies[name].versions"
           :key="info.version"
         >
           <el-tag type="success">{{ info.version }}</el-tag>
         </div>
       </div>
       <h3>devDependencies</h3>
-      <div class="version-item" v-for="name in devDependencies" :key="name">
-        <div class="name">{{ name }}</div>
+      <div
+        class="version-item"
+        v-for="info in formatValues(devDependencies)"
+        :key="info.name"
+      >
+        <div class="name">
+          <span>{{ info.name }}</span>
+          <el-tag type="primary">{{ info.version }}</el-tag>
+        </div>
         <div
           class="version"
-          v-for="info in ata.dependencies[name]"
-          :key="info.version"
+          v-for="v in formatValues(info.versions)"
+          :key="v.version"
         >
-          <el-tag type="success">{{ info.version }}</el-tag>
+          <el-tag type="success">{{ v.version }}</el-tag>
         </div>
       </div>
     </div>
@@ -51,7 +63,7 @@ const devDependencies = computed(() => data.value.dev_dependencies || {});
 const socket = new Socket();
 
 onMounted(() => {
-  socket.connect("ws://127.0.0.1:8080/ws/");
+  socket.connect("ws://127.0.0.1:8080/ws");
   socket.on("onmessage", handleReceiveData);
   socket.on("onopen", () => {
     ElMessage.success("连接成功!");
@@ -67,6 +79,13 @@ onMounted(() => {
  * 接收到数据
  */
 function handleReceiveData(data) {
+  data = JSON.parse(data.data);
+
   console.log(data);
+  appStore.updatePakage(data);
+}
+
+function formatValues(obj) {
+  return Object.values(obj);
 }
 </script>
