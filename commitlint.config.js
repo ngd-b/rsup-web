0fa694c9c44226fa572b511c48c9d81ae4f13452
hboot <bobolity@163.com>
2025-01-05 14:46:01 +0800

    chore: 手动触发工作流

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,17 @@ export default {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build"],
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "chore",
+      ],
     ],
   },
 };
